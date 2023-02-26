cabaf408014b6986648fc033d838735e25753ffb
eesfernandes <eesf@hotmail.com>
2023-02-25 23:35:15 -0300

    condicionais-I-template

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// Exercicio I
+// let booleano1 = false
+// let booleano2 = true
+// let booleano3 = true
+
+/* Condicionais da atividade I
+if(booleano1 === true){
+    alert(`O valor recebido pela variavel booleana1 é: ${booleano1}
+Portanto, booleano1 é === True - Condição verdadeira!`)
+}else{
+    alert(`O valor recebido pela variavel booleana1 é: ${booleano1}
+Portanto, booleano1 não é === True - Condição falsa!`)
+}
+*/
+
+/* Condicionais da atividade II
+if(booleano1 === booleano2){
+    alert(`Booleano1: ${booleano1} 
+Booleano2: ${booleano2} 
+São iguais.`)
+}else if( booleano2 === booleano3){
+    alert(`Booleano2: ${booleano2} 
+Booleano3: ${booleano3} 
+São iguais.`)
+}else if(booleano1 === booleano3){
+    alert(`Booleano1: ${booleano1} 
+Booleano3: ${booleano3} 
+São iguais.`)
+}else{
+    alert(`Nenhuma das condições: ${booleano1} - ${booleano2} - ${booleano3} São iguais!`)
+}
+*/
+
+/*
+// Condicionais atividade III
+let idade = 60
+if(idade <= 15){
+    alert(`Idade: ${idade} anos - Não pode emitir título de eleitor. Menor de 16 anos!`)
+}else if(idade >= 16 && idade <= 17){
+    alert(`Idade: ${idade} anos - Caso facultativo. Não tem obrigado de votar!`)
+}else if(idade >=18 && idade <= 59){
+    alert(`Idade: ${idade} anos - Pode emitir título de eleitor. Maior de idade!`)
+}else{
+    alert(`Idade: ${idade} anos - Caso facultativo. Não tem mais obrigação de votar!`)
+}
+*/
+
+let media = 13
+if(media >= 5 && media <= 10){
+    console.log(`Média: ${media} - Você foi aprovado!`)
+}else if(media >= 3 && media < 5){
+    console.log(`Média: ${media} - Você está de recuperação!`)
+}else if(media < 3){
+    console.log(`Média: ${media} - Você foi reprovado!`)
+}else{
+    console.log(`Média: ${media} - A média inserida é inválida!
+Digite uma média entre 0 e 10.`)
+}
